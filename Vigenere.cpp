13bce6eff38cf99069766bb3cd137dedf19bc60a
calvin5338 <44057469+calvin5338@users.noreply.github.com>
2019-03-07 18:42:51 +0800

    [未完成] Create Vigenere.cpp

---
+++ Vigenere.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int shiftValue(string plaintext, string key);
+
+int main() {
+  string plaintext, key;
+  cout << "請輸入欲加密的明文:";
+  cin >> plaintext;
+  cout << "請輸入密鑰:";
+  cin >> key;
+  string ciphertext;
+}
+
+int shiftValue(string plaintext, string key) {
+  
+}
